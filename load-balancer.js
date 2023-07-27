447494cf5b6eb4cfae1d9bc14e50d92ed0e237b2
chisom promise <chimisepro@gmail.com>
2023-07-27 05:45:43 +0100

    feat: create a simple load balancer

---
+++ load-balancer.js
@@ -0,0 +1,61 @@
+import {createServer} from 'http';
+import httpProxy from 'http-proxy';
+import { resDecorator } from './utils/index.js';
+import Consul from 'consul';
+
+const PORT = process.env.PORT || process.argv[2] || 8080;
+
+
+const servers = [
+    {
+        urls: ['http://localhost:8000'],
+        matcher: String.raw`^\/api\/people\/byFirstName\/[a-d]{1}\w*$`,
+        index: 0
+    },
+    {
+        urls: ['http://localhost:8000'],
+        matcher: String.raw`^\/api\/people\/byFirstName\/[e-p]{1}\w*$`,
+        index: 0
+    },
+    {
+        urls: ['http://localhost:8002'],
+        matcher: String.raw`^\/api\/people\/byFirstName\/[q-z]{1}\w*$`,
+        index: 0
+    }
+]
+
+const getMatchedServer = (url) => {
+    let matchServer;
+    for (const server of servers) {
+        const regex = new RegExp(server.matcher, 'i');
+        if(regex.test(url)) {
+            server.index = (server.index + 1) % server.urls.length;
+            matchServer = server.urls[server.index];
+            break;
+        }   
+    }
+
+    return matchServer;
+}
+
+
+async function main() {
+    const proxy = httpProxy.createProxyServer()
+    const server = createServer((req, res) => {
+        const url = req.url;
+        resDecorator(res);
+        const target = getMatchedServer(url);
+        if(!target) {
+            return res.status(502).end('Bad Gateway')
+        }
+
+        proxy.web(req, res, {target});
+    })
+
+    server.listen(PORT, () => {
+        console.log('Load balancer running on port %d', PORT);
+    })
+}
+
+
+main().catch(err => console.log(err));
